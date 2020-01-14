d9462b156cb97eb3da04acd5e3f846ce55b5b599
yaejinlee93 <yaejinlee93@gmail.com>
2020-01-13 18:22:36 -0700

    Player and item
    Created a player that moves and jumps. Created item. There is a sound effect when the player eats (touch) the item.

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    bool isJump;
+    Rigidbody rigid;
+    AudioSource audio;
+
+    private void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump) {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor") {
+            isJump = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            itemCount++;
+            audio.Play();
+
+            other.gameObject.SetActive(false);
+        }
+
+    }
+}
