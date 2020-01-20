15d5e2cee09ff0fb83f9e4a3fa70df881702dbfe
yaejinlee93 <yaejinlee93@gmail.com>
2020-01-20 16:15:19 -0700

    Finished building
    Created stages, created UI (scoreboard), back to start.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalItemCount;
+    public int stage;
+    public Text stageCountText;
+    public Text playerCountText;
+
+    private void Awake()
+    {
+        stageCountText.text = "/ "+ totalItemCount.ToString();
+    }
+
+    public void GetItem(int count)
+    {
+        playerCountText.text = count.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            SceneManager.LoadScene(stage);
+        }
+    }
+}
